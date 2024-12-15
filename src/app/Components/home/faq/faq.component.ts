ff1718109370acfe6b9cab5a4ad89040155b362a
knowmiq <knowmiq@gmail.com>
2024-12-15 22:58:10 +0530

    Working On Add Some changes 15_12_2024

--- src/app/Components/home/faq/faq.component.ts
+++ src/app/Components/home/faq/faq.component.ts
@@ -1,23 +1,45 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { EmailService } from '../../../Services/Email/email.service';
 
 @Component({
   selector: 'app-faq',
   imports: [FormsModule],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrl: './faq.component.css',
 })
 export class FaqComponent {
   formData = {
     name: '',
     email: '',
     service: '',
-    message: ''
+    message: '',
   };
 
+  constructor(private emailService: EmailService) {}
+
   onSubmit() {
-    //console.log('Form Data Submitted:', this.formData);
-    // Here you can handle the form submission, such as sending the data to an API.
-    //alert('Quote request submitted successfully!');
+    // Transform form data into the required format
+    // const emailData = {
+    //   ToEmail: this.formData.email, // Replace with a dynamic or hardcoded recipient email
+    //   Subject: `Service Request from ${this.formData.name}`,
+    //   Body: `
+    //   Name: ${this.formData.name}
+    //   Email: ${this.formData.email}
+    //   Service: ${this.formData.service}
+    //   Message: ${this.formData.message}
+    // `,
+    // };
+
+    // this.emailService.sendEmail(emailData).subscribe(
+    //   (response) => {
+    //     //alert('Email sent successfully!');
+    //     console.log(response);
+    //   },
+    //   (error) => {
+    //     //alert('Failed to send email. Please try again.');
+    //     console.error(error);
+    //   }
+    // );
   }
 }
